45365fe2a5df0f775adc9d18178553966df678f0
IgnaColla <ignaciocollazo98@hotmail.com>
2024-11-12 18:48:40 -0300

    Renumeracion de SPs

--- src/entrega/16_Roles.sql
+++ src/entrega/16_Roles.sql
@@ -56,33 +56,3 @@ EXECUTE AS USER = 'UserEmpleado';
 EXEC Ventas.InsertarNotaCredito '101-81-4070';
 REVERT;
 GO
-
-
---------------------------------------------------------------
--------------------  Encriptación de Datos -------------------
---------------------------------------------------------------
-
-USE Com2900G17;
-GO
-
--- Crear una clave maestra de base de datos
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Sc4ryM0v13!';
-
--- Crear un certificado para encriptación
-CREATE CERTIFICATE CertificadoEmpleado
-WITH SUBJECT = 'Encriptacion_empleados';
-GO
-
-ALTER TABLE Administracion.Empleado
-ADD DNI_encriptado VARBINARY(MAX), Direccion_encriptada VARBINARY(MAX);
-
--- Encriptar los datos existentes
-UPDATE Administracion.Empleado
-SET DNI_encriptado = ENCRYPTBYCERTBYKEY(CertificadoEmpleado, DNI),
-    Direccion_encriptada = ENCRYPTBYCERTBYKEY(CertificadoEmpleado, Direccion);
-
--- Eliminar las columnas no encriptadas
--- ALTER TABLE Administracion.Empleado
--- DROP COLUMN DNI, Direccion;
-
-GO
